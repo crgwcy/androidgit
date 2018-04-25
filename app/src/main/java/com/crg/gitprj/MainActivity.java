a328541ef73e08a75d4aba9526fb011462cfdcbf
crgwcy <893310656@qq.com>
2018-04-25 12:20:19 +0800

    add qmui lib butterfly and tab bar test!!!

--- app/src/main/java/com/crg/gitprj/MainActivity.java
+++ app/src/main/java/com/crg/gitprj/MainActivity.java
@@ -1,16 +1,86 @@
 package com.crg.gitprj;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.qmuiteam.qmui.util.QMUIResHelper;
+import com.qmuiteam.qmui.widget.QMUITabSegment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.tabs) QMUITabSegment mTabSegment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+
         setContentView(R.layout.activity_main);
+
+        initTabs();
+
+        if (savedInstanceState == null)
+        {
+      //      BaseFragment fragment = new HomeFragment();
+
+        }
     }
+    private void initTabs() {
+        int normalColor = QMUIResHelper.getAttrColor(this, R.attr.qmui_config_color_gray_6);
+        int selectColor = QMUIResHelper.getAttrColor(this, R.attr.qmui_config_color_blue);
+        mTabSegment.setDefaultNormalColor(normalColor);
+        mTabSegment.setDefaultSelectedColor(selectColor);
+//        mTabSegment.setDefaultTabIconPosition(QMUITabSegment.ICON_POSITION_BOTTOM);
+
+//        // 如果你的 icon 显示大小和实际大小不吻合:
+//        // 1. 设置icon 的 bounds
+//        // 2. Tab 使用拥有5个参数的构造器
+//        // 3. 最后一个参数（setIntrinsicSize）设置为false
+//        int iconShowSize = QMUIDisplayHelper.dp2px(getContext(), 20);
+//        Drawable normalDrawable = ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_component);
+//        normalDrawable.setBounds(0, 0, iconShowSize, iconShowSize);
+//        Drawable selectDrawable = ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_component_selected);
+//
+//        selectDrawable.setBounds(0, 0, iconShowSize, iconShowSize);
+//
+//        QMUITabSegment.Tab component = new QMUITabSegment.Tab(
+//                normalDrawable,
+//                normalDrawable,
+//                "Components", false, false
+//        );
+
+  /*      QMUITabSegment.Tab component = new QMUITabSegment.Tab(
+                ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_component),
+                ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_component_selected),
+                "Components", false
+        );
+    */
+        QMUITabSegment.Tab util = new QMUITabSegment.Tab(
+                ContextCompat.getDrawable(this, R.mipmap.icon_tabbar_util),
+                ContextCompat.getDrawable(this, R.mipmap.icon_tabbar_util_selected),
+                "Helper", false
+        );
+        QMUITabSegment.Tab lab = new QMUITabSegment.Tab(
+                ContextCompat.getDrawable(this, R.mipmap.icon_tabbar_lab),
+                ContextCompat.getDrawable(this, R.mipmap.icon_tabbar_lab_selected),
+                "Lab", false
+        );
+        QMUITabSegment.Tab zhibo = new QMUITabSegment.Tab(
+                ContextCompat.getDrawable(this, R.mipmap.icon_tabbar_lab),
+                ContextCompat.getDrawable(this, R.mipmap.icon_tabbar_lab_selected),
+                "zhibo", false
+        );
+        mTabSegment.addTab(zhibo)
+               /* .addTab(component)*/
+                .addTab(util)
+                .addTab(lab);
+    }
+
 
     @Override
     protected void onResume() {
